90af3ba10b7bc05f17ee47fab82934fe95f844fd
nibras154 <nibras_suryana_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:55:02 +0700

    Add files via upload

---
+++ ibra.java
@@ -0,0 +1,6 @@
+public class ibra {
+    public static void main(String[] args) {
+        System.out.printIn("nama saya ibra dari kelas X RPL 7byang berasal dari lamongan");
+        String a = "1";
+    }
+}
