acfd58a3dcd56f90dea3e1b1f851fa57f2faade2
nibras154 <nibras_suryana_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 12:41:05 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main (String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "ibra";
+        int usia = 15;
+        String alamat = "Jl. Danau Ranau 11";
+        double kelas = 10.7;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("usia : " + usia);
+        System.out.println("alamat : " + alamat);
+        System.out.println("kelas : " + kelas);
+    }
+}
