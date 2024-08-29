e77685af9b11904263123866aa018a70a15a47b6
nibras154 <nibras_suryana_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 14:44:41 +0700

    Add files via upload

--- Karyawan.java
+++ Karyawan.java
@@ -1,7 +1,41 @@
 //Object Class
 public class Karyawan {
     int id;
-    String nama, unit, alamat;
+    String nama;
+    String unit;
+    String alamat;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUnit() {
+        return this.unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
 
     //Constructor Parameter
     public Karyawan(int id, String nama, String unit, String alamat) {
--- Siswa.java
+++ Siswa.java
@@ -4,6 +4,30 @@ public class Siswa {
      String nama;
      double ipk;
 
+     public int getId() {
+          return this.id;
+     }
+
+     public void setId(int id) {
+          this.id = id;
+     }
+
+     public String getNama() {
+          return this.nama;
+     }
+
+     public void setNama(String nama) {
+          this.nama = nama;
+     }
+
+     public double getIpk() {
+          return this.ipk;
+     }
+
+     public void setIpk(double ipk) {
+          this.ipk = ipk;
+     }
+
      //Constructor
      //nama harus sama dgn Class
      public Siswa() {
@@ -11,4 +35,19 @@ public class Siswa {
         nama = "kosong";
         ipk = 0.0;
      }
+
+      //Constructor Parameter
+      public Siswa(int id, String nama, double ipk) {
+           //variabel = parameter
+           this.id = id;
+           this.nama = nama;
+           this.ipk = ipk;
+      }
+
+      //method print
+      public void print() {
+          System.out.println("ID : " + id);
+          System.out.println("Nama : " + nama);
+          System.out.println("IPK : " +ipk);
+      }
 }
